7d05bfe0520c64ebb176a5bd3d545347cc0acceb
syedsubhan17 <ssubhan431401@gamil.com>
2023-09-24 23:51:19 +0530

    creating simple Todo list

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.dir(document.body)
+// methods for retriving dom nodes
+// adding event listeners
+const node = document.getElementById('heading');
+node.innerHTML = 'Hello world from js';
+node.innerHTML = `
+<i> this is also a heading</i> <br>
+<b> this is bold from html<b>`
+console.log(node)
+console.dir(node)
