f81b157b74aed38f689b0ecac38e84495c9a2a3e
syedsubhan17 <ssubhan431401@gamil.com>
2023-09-25 00:40:04 +0530

    learned querySelector

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 // methods for retriving dom nodes
 // adding event listeners
 const node = document.getElementById('heading');
-node.innerHTML = 'Hello world from js';
-node.innerHTML = `
-<i> this is also a heading</i> <br>
-<b> this is bold from html<b>`
-console.log(node)
+// console.log(node.innerHTML)
+// lool both the get elementsbyId  line 3 is singular and line 6 is plural(using "s" cause it have multiple items)
+const listItem = document.getElementsByClassName('list-item');
+console.log(listItem); // this is returning the array in type of array
+console.log(node);
+console.log([1,2,3,])
+// you can use dir
 console.dir(node)
+console.dir(listItem)
+
+// querySelector - it default to macth the 1st which match
+const element = document.querySelector(`h1`)
+console.log(element)
+const element1 = document.querySelector(`li`)
+console.log(element1)
+
+// querySelectorALL - it select all the matching elemants
+const element2 = document.querySelectorAll(`li`) // this will give you a "Node list"
+console.log(element2)
