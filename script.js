4fd786dc8f97bb32210fe83382e80dcf86abfbe4
syedsubhan17 <ssubhan431401@gamil.com>
2023-09-25 17:56:44 +0530

    learnt seInterval and simple counter

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
 const heading = document.querySelector(`h1`)
 const body = document.querySelector(`body`)
 const content = document.querySelector(`p`)
-body.style = `
-margin:0;
-padding:0;
-box-sizing:border-box;
-`
-// console.log(heading.innerHTML)
-// console.log(body.innerHTML)
-//innner.html too see and access what in html 
 
 // now to make changes
-heading.innerHTML = "add stuff into heading"  // this 1 method to make changes
-heading.innerHTML = `
-    <span style="color: red">Hello</span> World! ! !
-`
+// heading.innerHTML = "add stuff into heading"  // this 1 method to make changes
+// heading.innerHTML = `
+//     <span style="color: red">Hello</span> World! ! !
+// `
+
 // console.log(heading.innerHTML) // this show output of what is inside heading
 // console.log(heading.textContent) // this show output what is text content init.
 
-// now adding style from js
-content.style = `
-    background:#f1f1f1;
-    padding:10px;
-    color:blueviolet;
-`
+// learning setInterval
+// const log = ()=>{
+//     console.log(`i'm a log`)
+// }
+
+// setInterval(log, 1000)
+
+// // in js we have global date
+// const date = new Date()
+// console.log(date) // this is teached on Thus 1 jun 23 19:54:16 // and iam learnig mon 25sep23 17:27:--
+
+// Lets create a counter
+let count = 0;
+const log = ()=>{
+    heading.innerHTML = count++
+}
+setInterval(log, 1000)
