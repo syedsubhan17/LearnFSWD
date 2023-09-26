013b5b8b4ad19a26cf70237acece71b0a0608a17
syedsubhan17 <ssubhan431401@gamil.com>
2023-09-26 19:02:54 +0530

    learnt tracking of keywords & mouse from user

--- script.js
+++ script.js
@@ -3,7 +3,20 @@ const textbox = document.querySelector(`h1 span`);
 const heading = document.querySelector(`h1`);
 
 let count = 0;
-heading.addEventListener('click',()=>{
-    console.log("you Clicked")
-    textbox.innerHTML = count++;
+heading.addEventListener('click',(event)=>{
+    console.log(event)
+    textbox.innerText = ++count;
 })
+
+//  this will track keyword pressed in the body
+// body.addEventListener('keydown',(event)=>{
+//     console.log(event.key)
+// })
+
+// this track every movement of mouse
+// body.addEventListener('mousemove',(event)=>{
+//     console.log({
+//         x: event.clientX,
+//         y: event.clientY,
+//     })
+// })
