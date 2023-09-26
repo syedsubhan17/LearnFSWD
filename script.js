687f209d3d0033422b57be95e7f33c52fd2f4cfa
syedsubhan17 <ssubhan431401@gamil.com>
2023-09-26 22:37:37 +0530

    leanrt tracking of copying and pasting

--- script.js
+++ script.js
@@ -1,27 +1,15 @@
 const progressbar = document.querySelector("#progress")
 const container = document.querySelector(".content")
 // console.log(progressbar)
-const contentHeight =  container.scrollHeight
 const html = document.querySelector(`html`)
 const scrollHeight = document.documentElement.scrollHeight
 
-console.log(html.clientHeight)
-// const bodyHeight = window.innerHeight
-// console.dir(container)
-
-//  this will increase length of progress bar as we scroll down
-window.addEventListener("scroll",()=>{
-    const scrollTop = window.scrollY
-    console.log(scrollTop) // this will give how mush you scrolled
-    const clientHeight = document.documentElement.clientHeight
-    const scrolled = scrollTop / (scrollHeight) * 100
-
-    
-    progressbar.style.width = `${scrolled}%`
+// to track coping the content
+window.addEventListener("copy",() => {
+    alert("please dont copy my content");
 })
-    // document.Element return you the HTML
-// look what document. do:
-console.log(document.documentElement)
-console.log(document.querySelector(`html`))
 
-console.log(document.documentElement.scrollHeight) // it will give you whole height
+// to track pasting
+window.addEventListener("paste",() => {
+    alert("please dont paste");
+})
