42a3b372b2db053e2e4cccc0aa4c3178b53bcd32
syedsubhan17 <ssubhan431401@gamil.com>
2023-09-26 19:36:15 +0530

    learnt scroll tracking

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const body = document.querySelector(`body`);
 const textbox = document.querySelector(`h1 span`);
 const heading = document.querySelector(`h1`);
+const input = document.querySelector(`input`);
 
 let count = 0;
 heading.addEventListener('click',(event)=>{
@@ -8,15 +9,36 @@ heading.addEventListener('click',(event)=>{
     textbox.innerText = ++count;
 })
 
-//  this will track keyword pressed in the body
-// body.addEventListener('keydown',(event)=>{
-//     console.log(event.key)
+// lets learn about event listener focus:
+// input.addEventListener('focus',(e)=>{
+//     console.log('input is focused')
 // })
 
-// this track every movement of mouse
-// body.addEventListener('mousemove',(event)=>{
-//     console.log({
-//         x: event.clientX,
-//         y: event.clientY,
-//     })
+// focus in and out:
+// input.addEventListener('focusin',(e)=>{
+//     console.log('input is focused')
+
 // })
+// input.addEventListener('focusout',(e)=>{
+//     console.log('input is out of focus')
+// })
+
+//  this can track the tab change
+window.addEventListener('focus',(e)=>{
+    console.log('tab changed detected')
+})
+
+// another event listener that is blur:
+window.addEventListener('blur',(e)=>{
+    console.log('blur changed detected')
+})
+
+// another event listener resize
+window.addEventListener('resize',(e)=>{
+    console.log('resize detected') // this will track the resizing the window
+})
+
+// lets track the scroll 
+window.addEventListener('scroll',(e)=>{
+    console.log('scroll detected') // this will track the resizing the window
+})
