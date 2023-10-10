e3877d61a524e6f1668c49649a1372a186ac9b8f
syedsubhan17 <ssubhan431401@gamil.com>
2023-10-11 00:37:54 +0530

    look what is SetInterval

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-console.log(1)
-for (let i = 0; i < 1; i++)
-console.log(2)
-console.log(3)
+// console.log(1)
+// for (let i = 0; i < 100; i++){
+//     console.log('loop')
+// }
+// console.log(2)
+// console.log(3)
+
+// Let see set Time out function
+const cb = ()=>{
+    console.log('callback executed')
+}
+setTimeout(cb,5000) //it give output after set of interval
