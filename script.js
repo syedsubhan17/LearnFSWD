5839f2106057f9ca3171b1957e18a736b9169c94
syedsubhan17 <ssubhan431401@gamil.com>
2023-10-11 01:46:58 +0530

    check the out put

--- script.js
+++ script.js
@@ -5,18 +5,40 @@
 // console.log(2)
 // console.log(3)
 
-console.log(1)
+// console.log(1)
 
-setInterval(()=>{
-    console.log(4)
-},0)
+// setInterval(()=>{
+//     console.log(4)
+// },0)
 
-setTimeout(()=>{
-    console.log(2)
-},0)
+// setTimeout(()=>{
+//     console.log(2)
+// },0)
 
-for(let i = 0; i < 100; i++ ){ //use 1k or 10k instead of 100
-    console.log('loop')
-}
+// for(let i = 0; i < 100; i++ ){ //use 1k or 10k instead of 100
+//     console.log('loop')
+// }
 
-console.log(3)
+// console.log(3)
+
+// create stack
+// function a() {
+//     throw new Error('this is an Error')
+// }
+
+// function b() {
+//     console.log('b')
+//     a()
+// }
+
+// function c() {
+//     console.log('c')
+//     b()
+// }
+
+// c()
+
+setTimeout(()=>console.log('hi'),1000) //1k = 1sec
+setTimeout(()=>console.log('bye'),2000)// this is 2sec 
+
+console.log('12e')
