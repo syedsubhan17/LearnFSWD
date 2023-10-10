34911b8af110a1ede884e109cb307da5fff16616
syedsubhan17 <ssubhan431401@gamil.com>
2023-10-11 01:27:39 +0530

    learnt between JS times and loop

--- script.js
+++ script.js
@@ -6,11 +6,17 @@
 // console.log(3)
 
 console.log(1)
-setInterval(()=>{
-    console.log(2)
-},5000)
-console.log(3)
 
-setTimeout(()=>{
+setInterval(()=>{
     console.log(4)
 },0)
+
+setTimeout(()=>{
+    console.log(2)
+},0)
+
+for(let i = 0; i < 100; i++ ){ //use 1k or 10k instead of 100
+    console.log('loop')
+}
+
+console.log(3)
