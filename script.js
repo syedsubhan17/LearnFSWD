d4622c779290d2cd82fb5ea816eeef4584050d6c
syedsubhan17 <ssubhan431401@gamil.com>
2023-10-12 01:44:31 +0530

    learn THEN and CATCH in promises

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-const response = axios.get('https://jsonplaceholder.typicode.com/todos')
-console.log(response)
+// const response = axios.get('https://jsonplaceholder.typicode.com/todos')
+// console.log(response)
 
-// here we use "then" learn to use
-response.then((data)=>{console.log(data)})
+// response
+// .then((data)=>{console.log(data)})
+// // if we get error and "then is getting rejecting"
+// .catch((err)=>{console.log('Do something')})
 
-setTimeout(()=>{
-    console.log(response)
-},1000)
+// setTimeout(()=>{
+//     console.log(response)
+// },1000)
+
+const fetchTodos = () => new Promise((resolve,reject) => {
+    setTimeout(()=>{
+        // resolve([{id:1, title:'todo1'},{id:2, title:'todo2'}])
+        reject(new Error('someThing went wrong'))
+    },3000)
+})
+
+fetchTodos().then((data)=>{console.log(data)})
+.catch((err)=>{console.log(err)})
