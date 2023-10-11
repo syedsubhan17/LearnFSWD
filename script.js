cf2a11beea677d4c251b4440a45d80474a71c980
syedsubhan17 <ssubhan431401@gamil.com>
2023-10-12 02:10:09 +0530

    learnt Promises

--- script.js
+++ script.js
@@ -19,9 +19,16 @@ fetch('https://jsonplaceholder.typicode.com/todos')
 })
 */
 
+const ul = document.createElement('ul')
+document.body.appendChild(ul)
+
 fetch('https://jsonplaceholder.typicode.com/todos')
 .then(response=>response.json())
-.then(data=>console.log(data))
-//  you can aslo add a to stop the stop the execution
-// this concept is know as "Promise Chaining"
+.then(data=>{
+    data.forEach(todo=>{
+        const li = document.createElement('li')
+        li.innerHTML = todo.title
+        ul.appendChild(li)
+    })
+})
 .catch(err=>console.log(err))
