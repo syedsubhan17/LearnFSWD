472520385037f93df435a6be48eb459ac521d4d5
syedsubhan17 <ssubhan431401@gamil.com>
2023-10-12 02:03:02 +0530

    learn the concept of Promise Chaining

--- script.js
+++ script.js
@@ -10,10 +10,18 @@
 //     console.log(response)
 // },1000)
 
+/*
 fetch('https://jsonplaceholder.typicode.com/todos')
 .then(response=>{
     console.log(response)
     const data = response.json()
-    console.log(data)
     data.then(newData=>{console.log(newData)}) //give soo many arrays
 })
+*/
+
+fetch('https://jsonplaceholder.typicode.com/todos')
+.then(response=>response.json())
+.then(data=>console.log(data))
+//  you can aslo add a to stop the stop the execution
+// this concept is know as "Promise Chaining"
+.catch(err=>console.log(err))
