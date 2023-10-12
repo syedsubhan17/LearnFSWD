21b5b4a19aa0a98ed1b589d045bd213ac7c71233
syedsubhan17 <ssubhan431401@gamil.com>
2023-10-12 12:58:38 +0530

    learn awaiting time to load

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-const ul = document.createElement('ul')
-document.body.appendChild(ul)
+// const ul = document.createElement('ul')
+// document.body.appendChild(ul)
 
-fetch('https://jsonplaceholder.typicode.com/todos')
-.then(response=>response.json())
-.then(data=>{
-    data.forEach(todo=>{
-        const li = document.createElement('li')
-        li.innerHTML = todo.title
-        ul.appendChild(li)
-    })
-})
-.catch(err=>console.log(err))
+// fetch('https://jsonplaceholder.typicode.com/todos')
+// .then(response=>response.json())
+// .then(data=>{
+//     data.forEach(todo=>{
+//         const li = document.createElement('li')
+//         li.innerHTML = todo.title
+//         ul.appendChild(li)
+//     })
+// })
 
 const fetchData = async()=>{
     try {
+        console.log(3)
         const res = await fetch('https://jsonplaceholder.typicode.com/todos')
-    console.log(res)
+    console.log(4)
     const data = await res.json();
     console.log(data);
     }
@@ -25,5 +25,10 @@ const fetchData = async()=>{
     finally {
         console.log('finally')
     }
-}
+};
+console.log(1)
 fetchData()
+console.log(2)
+for(let i = 0; i < 100; i++){
+    console.log('blocker')
+}
